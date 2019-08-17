35edd77848516060f36ef61a278cfc2a35224f35
OVVO-Financial <ovvo.financialsystems@gmail.com>
2019-08-16 20:24:33 -0400

    NNS 4.5 Beta Routines

--- R/gvload.R
+++ R/gvload.R
@@ -4,12 +4,12 @@
 #' @importFrom graphics abline boxplot legend lines par plot points segments text matplot title axis mtext barplot hist strwidth
 #' @importFrom stats coef cor lm na.omit sd median complete.cases resid uniroot aggregate density hat qnorm model.matrix fivenum
 #' @importFrom utils globalVariables tail combn flush.console
-#' @importFrom matrixStats rowProds
 #' @importFrom data.table data.table
 #' @import data.table
 #' @import doParallel
 #' @import rgl
 #' @import stringr
+#' @import matrixStats
 
 
 .onLoad <- function(libname = find.package("NNS"), pkgname = "NNS"){
