77b20ba81aa4284b3568825647811de92a4a393b
OVVO-Financial <ovvo.financialsystems@gmail.com>
2019-08-26 17:14:54 -0400

    NNS 4.5 Beta Routines

--- R/Causation.R
+++ R/Causation.R
@@ -6,24 +6,24 @@
 #' @param y \code{NULL} (default) or a numeric vector with compatible dimsensions to \code{x}.
 #' @param factor.2.dummy logical; \code{TRUE} (default) Automatically augments variable matrix with numerical dummy variables based on the levels of factors.  Includes dependent variable \code{y}.
 #' @param tau options: ("cs", "ts", integer); Number of lagged observations to consider (for time series data).  Otherwise, set \code{(tau = "cs")} for cross-sectional data.  \code{(tau = "ts")} automatically selects the lag of the time series data, while \code{(tau = [integer])} specifies a time series lag.
-#' @param time.series logical; \code{FALSE} (default) If analyzing time series data with \code{tau = [integer]}, select \code{(time.series = TRUE)}.  Not required when \code{(tau = "ts")}.
 #' @param plot logical; \code{FALSE} (default) Plots the raw variables, tau normalized, and cross-normalized variables.
 #' @return Returns the directional causation (x ---> y) or (y ---> x) and net quantity of association.  For causal matrix, directional causation is returned as ([column variable] ---> [row variable]).  Negative numbers represent causal direction attributed to [row variable].
 #' @author Fred Viole, OVVO Financial Systems
 #' @references Viole, F. and Nawrocki, D. (2013) "Nonlinear Nonparametric Statistics: Using Partial Moments"
 #' \url{http://amzn.com/1490523995}
 #' @examples
-#' ## x clearly causes y...
+#'
 #' \dontrun{
+#' ## x causes y...
 #' set.seed(123)
-#' x <- rnorm(100) ; y <- x ^ 2
+#' x <- rnorm(1000) ; y <- x ^ 2
 #' NNS.caus(x, y, tau = "cs")
 #'
-#' x <- 1:100 ; y <- x^2
-#' NNS.caus(x, y, tau = "ts", time.series = TRUE)
+#' ## AirPassengers
+#' NNS.caus(1:length(AirPassengers), AirPassengers, tau = "ts")
 #'
-#' ## Causal matrix
-#' NNS.caus(data.matrix(iris), tau = 0)
+#' ## Causal matrix with per factor causation
+#' NNS.caus(iris, tau = 0)
 #' }
 #' @export
 
@@ -83,18 +83,9 @@ NNS.caus <- function(x, y,
     }
 
     if(tau == "ts"){
-        Causes.xy <- numeric()
-        Causes.yx <- numeric()
+        Causation.y.given.x <- Uni.caus(y, x, tau = 3, plot = FALSE, time.series = TRUE)
+        Causation.x.given.y <- Uni.caus(x, y, tau = 3, plot = FALSE, time.series = TRUE)
 
-        for(i in 0 : 4){
-        # Populate scaling taus and calculate uni causation
-            Causes.xy[i] <- Uni.caus(x, y, tau = ceiling((2 ^ i) / 100 * length(x)), plot = FALSE, time.series = TRUE) / (2 ^ i)
-            Causes.yx[i] <- Uni.caus(y, x, tau = ceiling((2 ^ i) / 100 * length(x)) , plot = FALSE, time.series = TRUE) / (2 ^ i)
-        }
-
-        Causation.x.given.y <- mean(Causes.xy)
-
-        Causation.y.given.x <- mean(Causes.yx)
     }
 
 
@@ -105,9 +96,9 @@ NNS.caus <- function(x, y,
                 tau <- 0
             }
             if(tau == "ts"){
-                tau <- ceiling(0.03 * length(x))
+                tau <- 3
             }
-            Uni.caus(y, x, tau = tau, plot = plot)
+            Uni.caus(x, y, tau = tau, plot = plot)
         }
         return(c(Causation.x.given.y = Causation.x.given.y,
                Causation.y.given.x = Causation.y.given.x,
@@ -119,7 +110,7 @@ NNS.caus <- function(x, y,
                 tau <- 0
             }
             if(tau == "ts"){
-                tau <- ceiling(0.03 * length(x))
+                tau <- 3
             }
             Uni.caus(x, y, tau = tau, plot = plot)
         }
--- R/Uni_Causation.R
+++ R/Uni_Causation.R
@@ -1,37 +1,9 @@
 Uni.caus <- function(x, y, tau, plot = TRUE, scale = FALSE, time.series = FALSE){
 
-  xy <- NNS.norm(cbind(x, y), linear = TRUE)
+  xy <- NNS.norm(cbind(x, y), linear = FALSE)
 
   NNS.x <- xy[ , 1]
   NNS.y <- xy[ , 2]
-
-
-
-  if(mean((NNS.x)) != mean(NNS.y)){
-    xy <- apply(cbind(x, y), 2, function(b) (b - min(b)) / (max(b) - min(b)))
-    x <- xy[ , 1]
-    y <- xy[ , 2]
-    if( time.series | scale |
-        LPM.ratio(2, mean(x), x) < .25 | LPM.ratio(2, mean(y), y) < .25 |
-        LPM.ratio(2, mean(x), x) > .75 | LPM.ratio(2, mean(y), y) > .75
-    )
-    {
-      x <- scale(x)[ , 1]
-      y <- scale(y)[ , 1]
-    }
-  }
-
-  if(mean(NNS.x) == mean(NNS.y)){
-      x <- NNS.x
-      y <- NNS.y
-  if( time.series | scale |
-      LPM.ratio(2, mean(x), x) < .25 | LPM.ratio(2, mean(y), y) < .25 |
-      LPM.ratio(2, mean(x), x) > .75 | LPM.ratio(2, mean(y), y) > .75
-      ){
-      x <- scale(x)[ , 1]
-      y <- scale(y)[ , 1]
-  }
-}
 
   min.length <- min(length(x), length(y))
 
@@ -40,29 +12,30 @@ Uni.caus <- function(x, y, tau, plot = TRUE, scale = FALSE, time.series = FALSE)
 
   ## Create tau vectors
   if(tau > 0){
-    for (i in 0 : tau){
-      x.vectors[[paste('x.tau.', i, sep = "")]] <- numeric(0L)
-      y.vectors[[paste('y.tau.', i, sep = "")]] <- numeric(0L)
-      start <- tau - i + 1
-      end <- min.length - i
-      x.vectors[[i + 1]] <- x[start : end]
-      y.vectors[[i + 1]] <- y[start : end]
+    for (i in 0:tau){
+        x.vectors[[paste('x.tau.', i, sep = "")]] <- numeric(0L)
+        y.vectors[[paste('y.tau.', i, sep = "")]] <- numeric(0L)
+        start <- tau - i + 1
+        end <- min.length - i
+        x.vectors[[i + 1]] <- x[start : end]
+        y.vectors[[i + 1]] <- y[start : end]
     }
 
-    x.vectors.tau <- do.call(cbind,x.vectors)
-    y.vectors.tau <- do.call(cbind,y.vectors)
+      x.vectors.tau <- do.call(cbind,x.vectors)
+      y.vectors.tau <- do.call(cbind,y.vectors)
+
+      ## Normalize x to x.tau
+      x.norm.tau <- rowMeans(NNS.norm(x.vectors.tau))
+
+      ## Normalize y to y.tau
+      y.norm.tau <- rowMeans(NNS.norm(y.vectors.tau))
 
 
-    ## Normalize x to x.tau
-    x.norm.tau <- rowMeans(NNS.norm(x.vectors.tau))
-
-    ## Normalize y to y.tau
-    y.norm.tau <- rowMeans(NNS.norm(y.vectors.tau))
-  } else
-  {
-    x.norm.tau <- x
-    y.norm.tau <- y
+  } else {
+      x.norm.tau <- x
+      y.norm.tau <- y
   }
+
 
 
   ## Normalize x.norm.tau to y.norm.tau
@@ -76,7 +49,7 @@ Uni.caus <- function(x, y, tau, plot = TRUE, scale = FALSE, time.series = FALSE)
 
 
   ## Correlation of Normalized Variables
-  rho.x.y <- NNS.dep(x.norm.to.y, y.norm.to.x)$Dependence
+  rho.x.y <- NNS.dep(x.norm.to.y, y.norm.to.x)$Correlation
 
   Causation.x.given.y <- P.x.given.y * rho.x.y
 
