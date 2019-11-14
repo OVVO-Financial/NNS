c03c37496eb80c46d12bf9d00461c66e7a67f669
OVVO-Financial <ovvo.financialsystems@gmail.com>
2019-11-14 15:14:42 -0500

    NNS 4.7 Beta Routines

--- R/gvload.R
+++ R/gvload.R
@@ -20,7 +20,7 @@
       "mean_y_seg","mean_x_seg","sub.clpm",'sub.cupm','sub.dlpm','sub.dupm','weight','mean.x','mean.y',
       "Coefficient","X.Lower.Range","X.Upper.Range","y.hat","interval",
       "NNS.ID","max.x1","max.x2","min.x1","min.x2","counts",'old.counts',
-      "Period","Coefficient.of.Variation","Variable.Coefficient.of.Variation", "Sum", "j","lpm","upm",
+      "Period","Coefficient.of.Variation","Variable.Coefficient.of.Variation", "Sum", "j","lpm","upm", "tau",
       "i.x","i.y","q_new","x.x","x.y","standard.errors",
       "detectCores","makeCluster","registerDoSEQ","clusterExport",
       "%dopar%","foreach","stopCluster",
