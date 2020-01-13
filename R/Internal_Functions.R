4c528cf7fa6c2785f7ae98d7e64403af0a9093b6
OVVO-Financial <ovvo.financialsystems@gmail.com>
2020-01-13 14:45:12 -0500

    NNS 4.8.1 Beta Routines

--- R/Internal_Functions.R
+++ R/Internal_Functions.R
@@ -1,7 +1,7 @@
 ### Continuous Mode of a distribution
 mode <- function(x) {
   if(length(x) > 1){
-      d <- density(x)
+      d <- density(na.omit(x))
       d$x[which.max(d$y)]
   } else {
     x
