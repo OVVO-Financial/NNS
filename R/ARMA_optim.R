4501c672b06f1ef332280645c2eae18ae6fddd3f
OVVO-Financial <ovvo.financialsystems@gmail.com>
2020-02-06 15:25:17 -0500

    NNS 4.9 Beta Routines

--- R/ARMA_optim.R
+++ R/ARMA_optim.R
@@ -90,9 +90,9 @@ NNS.ARMA.optim <- function(variable, training.set,
       l <- training.set
   }
 
-  denominator <- min(5,max(2, as.integer(l/100)))
+  denominator <- min(4, max(2, as.integer(l/100)))
 
-  seasonal.factor <- seasonal.factor[seasonal.factor<=(l/denominator)]
+  seasonal.factor <- seasonal.factor[seasonal.factor <= (l/denominator)]
   seasonal.factor <- unique(seasonal.factor)
 
   if(length(seasonal.factor)==0){stop(paste0('Please ensure "seasonal.factor" contains elements less than ', l/denominator, ", otherwise use cross-validation of seasonal factors as demonstrated in the vignette >>> Getting Started with NNS: Forecasting"))}
