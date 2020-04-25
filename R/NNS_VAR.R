021977aa85e3b688e0af375911e77374111d6f9b
OVVO-Financial <ovvo.financialsystems@gmail.com>
2020-04-25 14:46:10 -0400

    NNS 5.2 Beta Routines

--- R/NNS_VAR.R
+++ R/NNS_VAR.R
@@ -328,8 +328,10 @@ NNS.VAR <- function(variables,
   forecasts <- data.frame(Reduce(`+`,list(t(t(nns_IVs_results)*uni) , t(t(nns_DVs)*multi))))
   colnames(forecasts) <- colnames(variables)
 
+  IE <- data.table(nns_IVs_interpolated_extrapolated)
+  colnames(IE) <- colnames(variables)
 
-  return( list("interpolated_and_extrapolated" = data.table(nns_IVs_interpolated_extrapolated),
+  return( list("interpolated_and_extrapolated" = IE,
               "relevant_variables" = data.frame(RV),
                univariate = nns_IVs_results,
                multivariate = nns_DVs,
