723884eb63d3eb515ef7f16c7d7bfc8aacaec05a
OVVO-Financial <ovvo.financialsystems@gmail.com>
2020-05-26 12:42:52 -0400

    NNS 5.3 Beta Routines

--- R/Multivariate_Regression.R
+++ R/Multivariate_Regression.R
@@ -180,7 +180,7 @@ NNS.M.reg <- function (X_n, Y, factor.2.dummy = FALSE, order = NULL, stn = NULL,
     }
   }
 
-  if(n.best > 1){
+  if(n.best > 1 && is.null(point.est)){
     if(!is.null(cl)){
         fitted.matrix$y.hat <- parallel::parApply(cl, original.IVs, 1, function(z) NNS::NNS.distance(REGRESSION.POINT.MATRIX, dist.estimate = z, type = dist, k = n.best)[1])
     } else {
