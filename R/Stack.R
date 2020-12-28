5da3ded3c95c91ed68346007c2346a54b831ce87
OVVO-Financial <ovvo.financialsystems@gmail.com>
2020-12-28 00:40:35 -0500

    NNS 5.7 Beta Routines

--- R/Stack.R
+++ R/Stack.R
@@ -389,7 +389,7 @@ NNS.stack <- function(IVs.train,
 
       if(b==folds){
         if(!is.null(type))  best.nns.cv <- min(1, mode(na.omit(unlist(best.nns.cv))))   else best.nns.cv <- mode(na.omit(unlist(best.nns.cv)))
-        best.k <- ceiling(gravity(as.numeric(rep(names(table(unlist(best.k))), table(unlist(best.k))))))
+        best.k <- ceiling(LPM.VaR(.625, 0, as.numeric(rep(names(table(unlist(best.k))), table(unlist(best.k))))))
         nns.method.1 <- NNS.reg(IVs.train[ , relevant_vars], DV.train, point.est = IVs.test[, relevant_vars], plot = FALSE, n.best = best.k, order = order, ncores = ncores,
                                 type = type, point.only = TRUE)$Point.est
         if(!is.null(type) && !is.null(nns.method.1)){
