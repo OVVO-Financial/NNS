1da28f955c99387ead2e479273f9130de795adb2
OVVO-Financial <ovvo.financialsystems@gmail.com>
2021-01-18 11:12:07 -0500

    NNS 6.0 Beta Routines

--- R/Dependence.R
+++ R/Dependence.R
@@ -90,7 +90,7 @@ NNS.dep = function(x,
     PART[, weights := .N/l, by = prior.quadrant]
     weights <- PART[, weights[1], by = prior.quadrant]$V1
 
-    ll <- expression(max(min(100, .N/5), 10))
+    ll <- expression(max(min(100, .N/5), 8))
 
     res <- PART[,  sign(cor(x[1:eval(ll)],y[1:eval(ll)]))*summary(lm(y[1:eval(ll)]~poly(x[1:eval(ll)],min(5, floor(log(l,4)+2)), raw = TRUE)))$r.squared, by = prior.quadrant]
     res[is.na(res)] <- 0
