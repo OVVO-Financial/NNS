6533f0c55ce9829d4eab97895d9fd6cb58c9ec50
OVVO-Financial <ovvo.financialsystems@gmail.com>
2021-01-31 13:16:17 -0500

    NNS 6.0 Beta Routines

--- R/NNS_Distance.R
+++ R/NNS_Distance.R
@@ -20,7 +20,8 @@ NNS.distance <- function(rpm, dist.estimate, type, k, n){
 
   if(type!="FACTOR"){
     rpm <- rbind(as.list(t(dist.estimate)), rpm[, .SD, .SDcols = 1:n])
-    rpm <- rpm[,names(rpm) :=lapply(.SD, function(b) (b - min(b)) / max(1e-10, (max(b) - min(b)))), .SDcols = 1:n]
+    rpm[, names(rpm) := lapply(.SD, as.numeric)]
+    rpm <- rpm[,lapply(.SD, function(b) (b - min(b)) / max(1e-10, (max(b) - min(b)))), .SDcols = 1:n]
     dist.estimate <- as.numeric(rpm[1, ])
     rpm <- rpm[-1,]
   }
@@ -63,9 +64,9 @@ NNS.distance <- function(rpm, dist.estimate, type, k, n){
 
   inv <- (1 / rpm$Sum)
   weights <- inv / sum(inv)
-  norm_weights <- dnorm(rpm$Sum, mean(rpm$Sum), sd(rpm$Sum))
-  norm_weights <- norm_weights / sum(norm_weights)
 
+  norm_weights <- pmin(max(rpm$Sum), dnorm(rpm$Sum, mean(rpm$Sum), sd(rpm$Sum)))
+  norm_weights <- norm_weights / sum(norm_weights)
   weights <- (weights + norm_weights)/2
 
   single.estimate <- sum(weights * rpm$y.hat)
