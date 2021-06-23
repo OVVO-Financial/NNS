d26103a2c711f4e86d8f4a3086aae822d73cb03d
OVVO-Financial <ovvo.financialsystems@gmail.com>
2021-06-23 09:52:05 -0400

    NNS 7.1 Beta Routines

--- R/NNS_Distance.R
+++ R/NNS_Distance.R
@@ -69,8 +69,8 @@ NNS.distance <- function(rpm, rpm_class, dist.estimate, type, k, n){
   emp <- rpm$Sum^(-1/min(k,l))
   emp_weights <- (emp + uni_weights) / sum(emp + uni_weights)
 
-  exp <- dexp(1:min(k,l), rate = 1/sqrt(min(k,l)))
-  exp_weights <- (exp + uni_weights) / sum(exp + uni_weights)
+  exp <- dexp(1:min(k,l), rate = 1/min(k,l))
+  exp_weights <- (exp ) / sum(exp )
 
 
   weights <- (emp_weights + exp_weights)/sum(emp_weights + exp_weights)
