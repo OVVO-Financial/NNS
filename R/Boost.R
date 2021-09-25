e425f8296f18fd65e14bebe052a71f36c842a764
OVVO-Financial <ovvo.financialsystems@gmail.com>
2021-09-25 11:30:39 -0400

    NNS 8.2 Beta Routines

--- R/Boost.R
+++ R/Boost.R
@@ -81,8 +81,8 @@ NNS.boost <- function(IVs.train,
 
   if(!is.null(type)){
     type <- tolower(type)
-    if(type=="class"){
-      obj.fn <- expression( mean(predicted == as.numeric(actual)) )
+    if(type == "class" && identical(obj.fn,expression( sum((predicted - actual)^2) ))){
+      obj.fn <- expression(mean( predicted == as.numeric(actual)))
       objective <- "max"
     }
   }
--- R/Stack.R
+++ R/Stack.R
@@ -108,7 +108,7 @@ NNS.stack <- function(IVs.train,
 
   if(!is.null(type)){
     type <- tolower(type)
-    if(type == "class"){
+    if(type == "class" && identical(obj.fn,expression( sum((predicted - actual)^2) ))){
       obj.fn <- expression(mean( predicted == as.numeric(actual)))
       objective <- "max"
     }
