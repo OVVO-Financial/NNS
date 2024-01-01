8670bfcd29d1e511e7f5bbc23db2eb406c71530b
OVVO-Financial <ovvo.financialsystems@gmail.com>
2024-01-01 14:28:07 -0500

    NNS 10.5 Btea

--- R/NNS_VAR.R
+++ R/NNS_VAR.R
@@ -367,7 +367,7 @@ NNS.VAR <- function(variables,
       equal_tau <- sum(given_var==observed_var)
       unequal_tau <- sum(given_var!=observed_var)
       
-      if(naive.weights) uni[i] <- mean(c(.5, equal_tau/(equal_tau + unequal_tau))) else uni[i] <- 0.5
+      if(!naive.weights) uni[i] <- mean(c(.5, equal_tau/(equal_tau + unequal_tau))) else uni[i] <- 0.5
       multi[i] <- 1 - uni[i]
     } else {
       uni[i] <- 0.5
