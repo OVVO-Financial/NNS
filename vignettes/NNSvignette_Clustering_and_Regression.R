3134f0703107fcbd764b25f203aca4a03f1d3a68
OVVO-Financial <ovvo.financialsystems@gmail.com>
2025-08-24 00:49:37 -0400

    NNS 11.6 Beta

--- vignettes/NNSvignette_Clustering_and_Regression.R
+++ vignettes/NNSvignette_Clustering_and_Regression.R
@@ -125,6 +125,9 @@ NNS.reg(cbind(x, x), y,
         residual.plot = TRUE,  
         ncores = 1, confidence.interval = .95)
 
+## ----smooth, fig.width=5,fig.height=3,fig.align = "center",results='hide'-----
+NNS.reg(x, y, smooth = T)
+
 ## ----uniimpute, eval=FALSE----------------------------------------------------
 #  set.seed(123)
 #  
@@ -224,9 +227,6 @@ NNS.reg(cbind(x, x), y,
 #         pch    = c(1, 15),
 #         pt.lwd = c(2, NA),
 #         bty    = "n")
-
-## ----smooth, fig.width=5,fig.height=3,fig.align = "center",results='hide'-----
-NNS.reg(x, y, smooth = T)
 
 ## ----threads, echo = FALSE----------------------------------------------------
 Sys.setenv("OMP_THREAD_LIMIT" = "")
